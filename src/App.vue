9e06d1e8133133287ad5d8de2950c5c7b6c8ff24
Aleks-Kar <Nomas86@yandex.ru>
2022-06-13 13:07:02 +0300

    create project, add peices components

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import TheKing from './components/Pieces/TheKing.vue'
+import TheQueen from './components/Pieces/TheQueen.vue'
+import TheRook from './components/Pieces/TheRook.vue'
+import TheBishop from './components/Pieces/TheBishop.vue'
+import TheKnight from './components/Pieces/TheKnight.vue'
+import ThePawn from './components/Pieces/ThePawn.vue'
+</script>
+
+<template>
+  <!-- <TheKing color="black" /> -->
+  <!-- <TheQueen color="black" /> -->
+  <!-- <TheRook color="white" /> -->
+  <!-- <TheBishop color="white" /> -->
+  <TheKnight color="white" />
+  <!-- <ThePawn color="white" /> -->
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: hsl(210, 29%, 24%);
+  margin-top: 60px;
+}
+</style>
