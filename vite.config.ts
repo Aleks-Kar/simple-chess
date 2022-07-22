878a25deed60529ffb67069e2323006fab974942
Aleks-Kar <Nomas86@yandex.ru>
2022-07-22 18:59:15 +0300

    chore: edited vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import * as path from "path";
 process.env.BROWSER = "chrome";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/simple-chess/",
   plugins: [vue()],
   server: {
     open: "/index.html",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+    },
+  },
+  build: {
+    target: "es2020",
+    outDir: "docs",
+    emptyOutDir: true,
+    sourcemap: true,
   },
 });
